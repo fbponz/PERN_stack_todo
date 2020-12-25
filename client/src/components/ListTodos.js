46fe7492d80a211e4f9e4d4b0b31fe878d3158df
Borja Ponz <fbponz@protonmail.ch>
2020-12-25 12:58:08 +0100

    Started to implement Modal

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,5 +1,7 @@
 import React, {Fragment , useEffect, useState} from "react";
 
+import EditTodo from "./EditTodo";
+
 const ListTodos = () => {
 
     const[todos, setTodos] = useState([]);
@@ -51,7 +53,9 @@ const ListTodos = () => {
             {todos.map(todo => (
                 <tr key={todo.todo_id}>
                     <td>{todo.description}</td>
-                    <td>Edit</td>
+                    <td>
+                        <EditTodo />
+                    </td>
                     <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                 </tr>
             ))}
